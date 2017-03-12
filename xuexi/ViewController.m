b24242fb0b5d053406ee8f626533a70251a20b39
mac <mac@macdemac-mini.lan>
2017-03-12 15:49:44 +0800

    通过calayer对视图进行修饰

--- xuexi/ViewController.m
+++ xuexi/ViewController.m
@@ -7,46 +7,74 @@
 //
 
 #import "ViewController.h"
-@interface ViewController ()<UIPickerViewDataSource, UIPickerViewDelegate>
+@interface ViewController ()
 
 //@property (nonatomic, strong) UIView *testView;
 //@property (nonatomic, strong) UIButton *button;
-
+@property (nonatomic,strong)UIButton * btn;
+@property (nonatomic,strong)UIButton * btnTwo;
+@property (nonatomic,strong)UIButton * btnThree;
+@property (nonatomic,strong)UIButton * btnFour;
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIPickerView *picker =[[UIPickerView alloc]initWithFrame:CGRectMake(50, 100, 280, 150)];
-    picker.delegate=self;
-    picker.dataSource=self;
-    [self.view addSubview:picker];
+    [self.view addSubview:self.btn];
+    [self.view addSubview:self.btnTwo];
+    [self.view addSubview:self.btnThree];
+    [self.view addSubview:self.btnFour];
+}
+-(UIButton *)btn{
+    if (!_btn)
+    {
+        _btn=[UIButton buttonWithType:UIButtonTypeCustom];
+        _btn.frame=CGRectMake(100, 100, 100, 100);
+        _btn.backgroundColor=[UIColor redColor];
+        _btn.layer.masksToBounds=YES;//就是圆角开关
+        _btn.layer.cornerRadius=10;//半径
+    }
+    return _btn;
 }
 
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{//设置分区数
-    return 2;
+-(UIButton *)btnTwo{
+    if (!_btnTwo)
+    {
+        _btnTwo=[UIButton buttonWithType:UIButtonTypeCustom];
+        _btnTwo.frame=CGRectMake(50, 50, 50, 50);
+        _btnTwo.backgroundColor=[UIColor greenColor];
+        _btnTwo.layer.masksToBounds=YES;
+        _btnTwo.layer.cornerRadius=25;
+    }
+    return _btnTwo;
 }
--(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{//设置分区行数
-    return 10;
-}
--(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return [NSString stringWithFormat:@"%lu 分区%lu 数据",component,row];//设置分区forcomponent 每一行的数据 component 和row是行列
-    
-    
 
-
+-(UIButton *)btnThree{
+    if (!_btnThree)
+    {
+        _btnThree=[UIButton buttonWithType:UIButtonTypeCustom];
+        _btnThree.frame=CGRectMake(200, 50, 50, 50);
+        _btnThree.backgroundColor=[UIColor yellowColor];
+        _btnThree.layer.borderColor=[UIColor grayColor].CGColor;//边框颜色
+        _btnThree.layer.borderWidth=2;//边框大小
+    }
+    return _btnThree;
 }
--(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component{
-    return 44;
-}
--(CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component{
-    return  140;
-    
-}
--(void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
-    NSLog(@"%lu,%lu",row,component);
+-(UIButton *)btnFour{
+    if (!_btnFour)
+    {
+        _btnFour=[UIButton buttonWithType:UIButtonTypeCustom];
+        _btnFour.frame=CGRectMake(150, 250, 50, 50);
+        _btnFour.backgroundColor=[UIColor orangeColor];
+        _btnFour.layer.shadowColor=[UIColor grayColor].CGColor;//阴影颜色
+        _btnFour.layer.shadowOffset=CGSizeMake(10, 10);//阴影位置
+        _btnFour.layer.shadowOpacity=1;//透明度
+    }
+    return _btnFour;
 }
 
 
