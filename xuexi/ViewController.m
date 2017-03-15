81f7f42eb5cf80caf910cc030ce8cb7f7b2331a9
mac <mac@macdemac-mini.lan>
2017-03-15 22:57:00 +0800

    UIActionSheet

--- xuexi/ViewController.m
+++ xuexi/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-@interface ViewController ()<UISearchBarDelegate,UIAlertViewDelegate>
+@interface ViewController ()<UIActionSheetDelegate>
 
 //@property (nonatomic, strong) UIView *testView;
 //@property (nonatomic, strong) UIButton *button;
@@ -23,28 +23,16 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    UIDatePicker * dataPicke=[[UIDatePicker alloc]initWithFrame:CGRectMake(50, 100, 280, 150)];
-    dataPicke.datePickerMode=UIDatePickerModeTime;//time时间模式 Date日期模式 DateAndTime 日期和时间模式 CountDownTimer 计时模式
-    [dataPicke addTarget:self action:@selector(change:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:dataPicke];
-}
--(void)change:(UIDatePicker *)picker{
-    NSLog(@"%@",picker.date);
-
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-
-    UIAlertView * alert =[[UIAlertView alloc]initWithTitle:@"标题" message:@"内容" delegate:self  cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
-    [alert show];
+    UIActionSheet * actionSheet=[[UIActionSheet alloc]initWithTitle:@"标题" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"删除" otherButtonTitles:@"确定", nil];
+    [actionSheet showInView:self.view];
 }
--(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
     NSLog(@"click");
 }
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
